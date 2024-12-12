336fae0e93100c6ae52b847d9dbeefb0257a2819
davidnoya <dnoyav23@fpcoruna.afundacion.org>
2024-12-12 12:48:03 +0100

    Primer commit pokemon

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Pokemon from './components/pokemon';
+
+function App() {
+  return (
+    <Pokemon></Pokemon>
+  );
+}
+
+export default App;
